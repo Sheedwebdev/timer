cc22c4583a69c465f8cda1057dfb326254fb49b2
Rashid Enahora <rashidenahora@Rashids-MacBook-Pro.local>
2025-11-15 16:23:15 -0500

    Added pause/unpause button to timer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,16 +7,41 @@ import styled, { createGlobalStyle } from 'styled-components';
 
     return (
       <Wrapper>
-        {shown && <Timer />}
-        <button onClick={() => setShown(!shown)}>{shown ? "Unmount" : "Mount"}</button>
+        <TimeCard>
+          {shown && <Timer />}
+          <Button onClick={() => setShown(!shown)}>{shown ? "Unmount" : "Mount"}</Button>
+        </TimeCard>
       </Wrapper>
     );
   }
 
-  const Wrapper = styled.div`
+  const Button = styled.button`
+    font-size: 1.25rem;
+    font-weight: 600;
+
+    &:hover {
+      background-color: hsl(0deg 0% 50%);
+      border: 5px solid hsl(0deg 0% 0%);
+      color: hsl(0deg 0% 100%);
+    }
+  `;
+
+  const TimeCard = styled.div`
     display: flex;
     flex-direction: column;
     gap: 30px;
+    justify-content: center;
+    align-items: center;
+    width: 350px;
+    height: 450px;
+    background-color: hsl(45deg 100% 50%);
+    border-radius: 15px;
+    border: 10px solid hsl(0deg 0% 0%);
+    padding: 20px;
+  `;
+
+  const Wrapper = styled.div`
+    display: flex;
     justify-content: center;
     align-items: center;
     width: 100vw;
--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 
   function Timer() {
     const [seconds, setSeconds] = useState(0);
+    const [isPaused, setIsPaused] = useState(false);
 
     useEffect(() => {
+      if (isPaused) return;
       console.log("Component Mounted - Timer Started...");
 
       const timerInterval = setInterval(() => {
@@ -15,12 +17,31 @@ import styled from 'styled-components';
       return () => {
         clearInterval(timerInterval);
       }
-    }, []);
+    }, [isPaused]);
+
+    function pauseToggle() {
+      setIsPaused(prev => !prev);
+    }
+
 
     return (
-        <h1>Seconds: {seconds}</h1>
+        <>
+          <h1>Seconds: {seconds}</h1>
+          <Button onClick={pauseToggle}>{isPaused ? "Unpause" : "Pause"}</Button>
+        </>
     );
   }
+
+  const Button = styled.button`
+    font-size: 1.25rem;
+    font-weight: 600;
+
+    &:hover {
+      background-color: hsl(0deg 0% 50%);
+      border: 5px solid hsl(0deg 0% 0%);
+      color: hsl(0deg 0% 100%);
+    }
+  `;
 
 
 
